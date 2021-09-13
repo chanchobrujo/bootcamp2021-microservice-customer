65668c57beeb005508a85501ebc639e1338dbe1c
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-09-12 21:54:39 -0500

    Update 2

--- src/main/java/com/everisbootcamp/customers/CustomersApplication.java
+++ src/main/java/com/everisbootcamp/customers/CustomersApplication.java
@@ -3,12 +3,16 @@ package com.everisbootcamp.customers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import lombok.extern.slf4j.Slf4j;
  
+@Slf4j
 @EnableEurekaClient
 @SpringBootApplication
 public class CustomersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomersApplication.class, args);
+        log.info("MICROSERVER CUSTOMER ENABLED");
     }
 }
