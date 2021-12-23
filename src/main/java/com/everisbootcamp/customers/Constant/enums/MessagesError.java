e1847240fc5e3f5441ec2f3362b02f4e97cfc6ba
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-12-23 14:34:25 -0500

    REFACTOR: Add enums for messages response.

---
+++ src/main/java/com/everisbootcamp/customers/Constant/enums/MessagesError.java
@@ -0,0 +1,18 @@
+package com.everisbootcamp.customers.Constant.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum MessagesError {
+	INCORRECT_DATA(400, "Datos incorrectos."),
+	REPETED_DATA(400, "Datos repetidos."),
+	NOTFOUND_DATA(404, "Datos no encontrados."),
+	;
+	private Integer cod;
+	private String messages;
+	
+	private MessagesError(Integer cod, String messages) {
+		this.cod = cod;
+		this.messages = messages; 
+	} 
+}
--- src/main/java/com/everisbootcamp/customers/Model/Response.java
+++ src/main/java/com/everisbootcamp/customers/Model/Response.java
@@ -7,19 +7,31 @@ import java.util.Map;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 
+import com.everisbootcamp.customers.Constant.enums.MessagesError;
+import com.everisbootcamp.customers.Constant.enums.MessagesSuccess;
+
 @Getter
-@Setter
-@AllArgsConstructor
+@Setter 
 @NoArgsConstructor
 public class Response {
 
     private String message;
     private HttpStatus status;
-    private LocalDateTime timestamp = LocalDateTime.now(ZoneId.of("America/Lima"));
+    private LocalDateTime timestamp = LocalDateTime.now(ZoneId.of("America/Lima"));  
 
     public Response(String message, HttpStatus status) {
         this.message = message;
         this.status = status;
+    }
+
+    public Response(MessagesSuccess message) {
+        this.message = message.getMessages();
+        this.status = HttpStatus.valueOf(message.getCod());
+    }
+
+    public Response(MessagesError message) {
+        this.message = message.getMessages();
+        this.status = HttpStatus.valueOf(message.getCod());
     }
 
     public Map<String, Object> getResponse() {
